581332cef2fb3618eadc8eb883d8e33ec2bd4a2e
Jefersson Sanchez <sanchezjs901@gmail.com>
2025-02-11 14:13:24 -0500

    Creación proyecto

---
+++ src/main/java/com/tickmanage/TickManageApplication.java
@@ -0,0 +1,13 @@
+package com.tickmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tickmanage/TickManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
